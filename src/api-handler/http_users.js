eb40a89593f8fee2001c766b9997614f13f6a0d5
Aslam <aslam17@navgurukul.org>
2020-08-05 15:45:39 +0530

    making request to login api

--- src/api-handler/http_users.js
+++ src/api-handler/http_users.js
@@ -1,10 +1,24 @@
 import axios from 'axios'
 const signUpUrl = '/user/api/contact-manager/v1/create/user'
+// login
+const signInUrL = '/user/api/contact-manager/v1/login/user'
 
 class users {
     // create new User
     static insertPost(data){
         return axios.post(`${signUpUrl}`, data)
+        .then(function(res){
+            return res
+        }).catch((err) =>{
+            // console.log(err.response)
+            throw(err)
+            // return err.response
+        })
+    }
+
+    // create new User
+    static Login(data){
+        return axios.post(`${signInUrL}`, data)
         .then(function(res){
             return res
         }).catch((err) =>{
